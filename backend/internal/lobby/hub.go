c2a0efbc1da47a179829dc1e0e9ea42a99994af1
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2026-03-11 00:35:28 -0300

    refactor: replace global hub singleton with HubStore interface and DI
    - Define HubStore interface (GetOrCreateRoom, Broadcast, RemoveClient)
    - Implement all three methods on *Hub
    - Add NewHub() constructor; remove package-level var hub singleton
    - RemoveClient deletes room from hub when last client leaves
    - Change map[*Client]bool to map[*Client]struct{} (idiomatic set)

--- backend/internal/lobby/hub.go
+++ backend/internal/lobby/hub.go
@@ -2,9 +2,15 @@ package lobby
 
 import "sync"
 
+type HubStore interface {
+	GetOrCreateRoom(name string) *Room
+	Broadcast(room *Room, msg []byte, exclude *Client)
+	RemoveClient(room *Room, c *Client)
+}
+
 type Room struct {
 	name        string
-	clients     map[*Client]bool
+	clients     map[*Client]struct{}
 	gameStarted bool
 	mutex       sync.RWMutex
 }
@@ -12,13 +18,7 @@ type Room struct {
 func (r *Room) addClient(client *Client) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	r.clients[client] = true
-}
-
-func (r *Room) removeClient(client *Client) {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	delete(r.clients, client)
+	r.clients[client] = struct{}{}
 }
 
 func (r *Room) broadcast(message []byte, exclude *Client) {
@@ -39,6 +39,37 @@ type Hub struct {
 	mutex sync.RWMutex
 }
 
-var hub = &Hub{
-	rooms: make(map[string]*Room),
+func NewHub() *Hub {
+	return &Hub{rooms: make(map[string]*Room)}
+}
+
+func (h *Hub) GetOrCreateRoom(name string) *Room {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	room, exists := h.rooms[name]
+	if !exists {
+		room = &Room{
+			name:    name,
+			clients: make(map[*Client]struct{}),
+		}
+		h.rooms[name] = room
+	}
+	return room
+}
+
+func (h *Hub) Broadcast(room *Room, msg []byte, exclude *Client) {
+	room.broadcast(msg, exclude)
+}
+
+func (h *Hub) RemoveClient(room *Room, c *Client) {
+	room.mutex.Lock()
+	delete(room.clients, c)
+	empty := len(room.clients) == 0
+	room.mutex.Unlock()
+
+	if empty {
+		h.mutex.Lock()
+		delete(h.rooms, room.name)
+		h.mutex.Unlock()
+	}
 }
